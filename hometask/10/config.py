0c6273e1ad8cbc84b90ebea46ccbcd94871707a0
Yura Drahula <ydrahula@gmail.com>
2022-06-21 10:07:32 +0300

    hometask 10

---
+++ hometask/10/config.py
@@ -0,0 +1,10 @@
+BASE_URL = 'https://gorest.co.in/public/v2/users'
+TOKEN = '18d9b2f39a546090ec67010cccf156db727de43a3d090e35dde0d7cfb42272e9'
+
+USERS_DATA = {
+  "email": "asaswqeilda33@mail.com",
+  "name": "Schmitt",
+  "gender": "male",
+  "status": "active"
+}
+
